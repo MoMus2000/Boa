3494868de7fdabd6079ec66eaff9cb8ef8a88c8f
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-11-23 23:59:57 -0500

    Write tests for the parser

---
+++ tests/test_parser.py
@@ -0,0 +1,29 @@
+from ..lexer import Lexer
+from ..parser import Parser
+from ..expr import *
+
+def test():
+    lexer = None
+    printer = AstPrinter()
+    tests = [
+        '"hi"',
+        "(+ 1 2)",
+        "(* (group (+ 4 5)) (group (+ 6 7)))",
+        "(* (group (- 5 3)) 2)",
+        "(- 2 (group (+ 3 (* 4 5))))",
+        "(+ (+ 3 (* 4 5)) 2)",
+    ]
+    with open("./tests/test_parser.boa", "r") as source:
+        for test, line in zip(tests, source.readlines()):
+            print(line)
+            lexer  = Lexer(line)
+            tokens = lexer.scan_tokens()
+            parser = Parser(tokens)
+            expression = parser.parse()
+            expression = printer.print(expression)
+
+            assert test == expression
+
+if __name__ == "__main__":
+    test()
+

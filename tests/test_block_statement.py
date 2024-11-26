f004331bf619bf637831c3fb2aff2853f8102b13
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-11-26 03:04:02 -0500

    Add block statement test

---
+++ tests/test_block_statement.py
@@ -0,0 +1,31 @@
+from ..lexer import Lexer
+from ..parser import Parser
+from interpreter import Interpreter
+
+def test():
+    lexer = None
+    interpreter = Interpreter()
+    tests = [
+        '"A"',
+        '"Hello World, my name is Mustafa"',
+        '"A"',
+        '"xxx"',
+        '"A"',
+        ['"www"', '"A"'],
+        '"xxx"',
+        '"A"',
+    ]
+    with open("./tests/test_block_statement.boa", "r") as source:
+            source_code = source.readlines()
+            lexer  = Lexer(source_code)
+            tokens = lexer.scan_tokens()
+            parser = Parser(tokens)
+            statements = parser.parse()
+            result = interpreter.interpret(statements)        
+            print(result)
+            for test, r in zip(tests, result):
+                assert r == test
+
+if __name__ == "__main__":
+    test()
+

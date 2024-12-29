6338c6714490cb9da55d42c5a66eee541a0cfd5c
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-12-29 06:44:04 -0500

    Modify test script

--- test.sh
+++ test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-PYTHONPATH=$(pwd) pytest tests -s -v
+PYTHONPATH=$(pwd) pytest tests -s -v -q
+
+# Find all .boa files in the tests directory
+files=$(find ./tests -type f -name "*.boa" ! -name "*lexer*" ! -name "*parser*" ! -name "*interpreter*")
+
+
+# Count the number of files
+count=$(echo "$files" | wc -l)
+
+# Print and process the files
+echo "Total .boa files found: $count"
+for file in $files; do
+  python3 main.py $file > /dev/null
+  echo -e "\033[32mSuccess: Processed $file\033[0m"
+done
